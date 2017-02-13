3366e05b3ae24092c6ed1f5cb437696d37045a68
dthuerck <daniel.thuerck@gmail.com>
2017-02-13 09:33:31 +0100

    Initial: adding all-in-one code, already tested.

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # ha-light-dioder
-IKEA dioder component for Home Assistant.
+
+A Home Assistant ([code on Github](https://github.com/home-assistant/home-assistant)
+component for interfacing with the IKEA Dioder LED system.
+
+## Hardware setup
+This code assumes the a setup using the following parts:
+* Raspberry Pi (here: Model B+, should be exchangeable, though)
+* Adafruit (or similar, e.g. SunFounder) PCA9685 12xPWM driver for I2C
+* ULN2003 Transistor array for accessing the Dioder's 12V supply
+
+Similar to [this Fritzing project](http://fritzing.org/projects/ikea-dioder-hack),
+we replace the Dioder's controller entirely. 
+
+Here's a short description of the setup: The PCA9685 is coupled to the Raspberry Pi
+via its native I2C interface; PWM ports 0 - 2 on the PCA9685 board are connected
+to inputs 0 - 2 of the ULN2003. On its output side, we connect the pins from
+DIODER's 12V supply for GND and V+ as well as the pins for R, G, B (in that
+order) to the ULN outputs 0 - 2 (photo to come).
+
+## Software setup
+To get the I2C bus running, the following steps have to be taken (for
+Home Assistant's Python VENV):
+* Install ``python3-smbus python-smbus`` (if necessary, copy package to HA's VENV python)
+* Add user homeassistant to group ``i2c``
+
+## Installation
+Assuming the HA/SW setup from above, moving the included ``pidioder.py`` to
+``/home/homeassistant/.homeassistant/custom_components/light/`` includes
+the component (assuming an All-in-One HA installation).
+
+Then, add the following lines to your ``configuration.yaml``:
+```
+light:
+    platform: pidioder
+```
+
+That's it! If everythin has worked out, you should have the following interface
+on your HA state screen:
+[HA interface](ha-interface.png)
---
+++ pidioder.py
@@ -0,0 +1,191 @@
+# Copyright (c) 2017, Daniel Thuerck
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from math import *
+import smbus
+import time
+import logging
+
+# Home assistant stuff - use class from above to setup light access
+from homeassistant.components.light import (
+    ATTR_RGB_COLOR, SUPPORT_RGB_COLOR, Light)
+
+_LOGGER = logging.getLogger(__name__)
+
+SUPPORT_PILIGHT = (SUPPORT_RGB_COLOR)
+
+# HA hook
+def setup_platform(hass, config, add_devices_callback, discovery_info=None):
+    # Just add one default PiDioder light with default address
+    add_devices_callback([PiDioderLight(0x40, _LOGGER)])
+
+# Representation of a polling-type light for HA, using the PiDioder class
+class PiDioderLight(Light):
+
+    def __init__(self, addr, log):
+        # initialize PiDioder light at I2C address and put to sleep
+        self._dev = PiDioder(addr, log)
+        self._dev.set_freq(1000)
+        self._is_sleep = True
+        self._dev.sleep(self._is_sleep)
+
+        # HA attributes
+        self._name = "PiDioder hinter TV"
+        self._state = not self._is_sleep
+        self._rgb = (255, 255, 255)
+
+    @property
+    def should_poll(self):
+        return False
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def available(self):
+        return True
+
+    @property
+    def is_on(self):
+        return not self._is_sleep
+
+    @property
+    def rgb_color(self):
+        return self._rgb
+
+    @property
+    def supported_features(self):
+        return SUPPORT_PILIGHT
+
+    def turn_on(self, **kwargs):
+        # turn on and set to chosen color (or all white)
+        self._is_sleep = False
+        self._dev.sleep(self._is_sleep)
+        self._state = not self._is_sleep
+        if ATTR_RGB_COLOR in kwargs:
+            self._rgb = kwargs[ATTR_RGB_COLOR]
+        self._dev.set_color((self._rgb[0] / 255.0, self._rgb[1] / 255.0, self._rgb[2] / 255.0))
+
+        # inform HA about state update
+        self.schedule_update_ha_state()
+
+    def turn_off(self, **kwargs):
+        self._dev.set_all_pwm(0)
+        self._is_sleep = True
+        self._dev.sleep(self._is_sleep)
+        self._state = not self._is_sleep
+
+        # inform HA about state update
+        self.schedule_update_ha_state()
+
+# Basic library - Access an IKEA dioder attached to pins 0-2 on a PCA9685 via I2C
+class PiDioder:
+
+    def __init__(self, addr, log):
+        self._bus = smbus.SMBus(1)
+        self._log = log
+
+        # Maximum value - should not be 4096, since that results in errors!
+        # 3900 \approx 0.99 * 4096, that works in my experience
+        self._maxval = 3900
+
+        # I2C stuff
+        self._DEVICE_ADDRESS = addr
+        self._SWRST = 0x06
+
+        # bit masks
+        self._SLEEP = 0x10
+        self._ALLCALL = 0x01
+
+        # PCA9685 register (LEDn register off/on are split in 2 x 8bit regs)
+        self._PRESCALE = 0xFE
+        self._MODE1 = 0x00
+        self._MODE2 = 0x01
+        self._LED0_ON_L = 0x06
+        self._LED0_ON_H = 0x07
+        self._LED0_OFF_L = 0x08
+        self._LED0_OFF_H = 0x09
+        self._ALL_LED_ON_L = 0xFA
+        self._ALL_LED_ON_H = 0xFB
+        self._ALL_LED_OFF_L = 0xFC
+        self._ALL_LED_OFF_H = 0xFD
+
+        # perform SW reset
+        self._bus.write_byte(0x00, self._SWRST)
+
+    def sleep(self, sleep_status):
+        # set register MODE1 to sleep and wait for activation
+        old_mode = self._bus.read_byte_data(self._DEVICE_ADDRESS, self._MODE1)
+        if sleep_status:
+            new_mode = old_mode | self._SLEEP
+        else:
+            new_mode = old_mode & ~self._SLEEP
+        self._bus.write_byte_data(self._DEVICE_ADDRESS, self._MODE1, new_mode)
+        time.sleep(0.005)
+
+
+    # set PWM frequency (min: 0x1E/200Hz, max: 0xFF/1526Hz)
+    def set_freq(self, f):
+        prescale = 25000000
+        prescale /= 4096.0
+        prescale /= float(f)
+        prescale -= 1
+        prescale = int(prescale)
+        if prescale > 3 and prescale <= 255:
+            # Frequency can only be changed while sleeping
+            self.sleep(True)
+            self._bus.write_byte_data(self._DEVICE_ADDRESS, self._PRESCALE, prescale)
+            self.sleep(False)
+
+    # set PWM channel value
+    def set_pwm(self, channel, val):
+        if val < 0 or val > 1:
+            return
+        start = 0
+        end = int(val * self._maxval)
+        self._bus.write_byte_data(self._DEVICE_ADDRESS, self._LED0_ON_L + 4 * channel, 0)
+        self._bus.write_byte_data(self._DEVICE_ADDRESS, self._LED0_ON_H + 4 * channel, 0)
+        self._bus.write_byte_data(self._DEVICE_ADDRESS, self._LED0_OFF_L + 4 * channel, end & 0xff)
+        self._bus.write_byte_data(self._DEVICE_ADDRESS, self._LED0_OFF_H + 4 * channel, end >> 8)
+
+    def set_all_pwm(self, val):
+        if val < 0 or val > 1:
+            return
+        start = 0
+        end = int(val * self._maxval)
+        self._bus.write_byte_data(self._DEVICE_ADDRESS, self._ALL_LED_ON_L, 0)
+        self._bus.write_byte_data(self._DEVICE_ADDRESS, self._ALL_LED_ON_H, 0)
+        self._bus.write_byte_data(self._DEVICE_ADDRESS, self._ALL_LED_OFF_L, end & 0xff)
+        self._bus.write_byte_data(self._DEVICE_ADDRESS, self._ALL_LED_OFF_H, end >> 8)
+
+    def set_color(self, rgb):
+        # Note: DIODERs are RBG instead of RGB, so switch last two channels
+        self.set_pwm(0, rgb[0])
+        self.set_pwm(1, rgb[2])
+        self.set_pwm(2, rgb[1])
